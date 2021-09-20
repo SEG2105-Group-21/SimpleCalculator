17322b4a45e6410ade571b9a34b0d729068ce043
davidswartz02 <71353640+davidswartz02@users.noreply.github.com>
2021-09-20 12:40:36 -0400

    Add files via upload

---
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPlus, btnMinus, btnTimes, btnDivide, btnClear, btnDecimal, btnEquals;
+    TextView display;
+    double val1, val2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.Btn0);
+        btn1 = findViewById(R.id.Btn1);
+        btn2 = findViewById(R.id.Btn2);
+        btn3 = findViewById(R.id.Btn3);
+        btn4 = findViewById(R.id.Btn4);
+        btn5 = findViewById(R.id.Btn5);
+        btn6 = findViewById(R.id.Btn6);
+        btn7 = findViewById(R.id.Btn7);
+        btn8 = findViewById(R.id.Btn8);
+        btn9 = findViewById(R.id.Btn9);
+        btnPlus = findViewById(R.id.BtnPlus);
+        btnMinus = findViewById(R.id.BtnMinus);
+        btnTimes = findViewById(R.id.BtnTimes);
+        btnDivide = findViewById(R.id.BtnDivide);
+        btnClear = findViewById(R.id.BtnClear);
+        btnDecimal = findViewById(R.id.BtnDecimal);
+        btnEquals = findViewById(R.id.BtnEquals);
+
+
+    }
+}
